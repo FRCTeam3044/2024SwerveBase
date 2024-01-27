3529c697776d127f9d42e2fe70e592307b073542
ghorrocks217 <76536843+ghorrocks217@users.noreply.github.com>
2024-01-27 12:34:24 -0500

    Added stuff that doesnt work

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,9 +1,21 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
 
 public class ElevatorSubsystem {
-    CANSparkMax elevatorMotor = new CANSparkMax(0, MotorType.kBrushless);
+    CANSparkMax elevatorMotor = new CANSparkMax();
     
+    AbsoluteEncoder shooterEncoder = new AbsoluteEncoder();
+
+    PIDController pid = new PIDController(0, 0, 0);
+
+    public void setAngleAdjustment() {
+
+    }
+
+    public void setToIntakeMode() {
+
+    }
 }
