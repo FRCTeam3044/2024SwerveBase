e5ab2843e146ddcbd18c270b956e36fb504fa763
ghorrocks217 <76536843+ghorrocks217@users.noreply.github.com>
2024-01-27 12:55:53 -0500

    Added some methods and classes

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,13 +1,22 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class ElevatorSubsystem {
-    CANSparkMax elevatorMotor = new CANSparkMax();
+    CANSparkMax elevatorMotorOne = new CANSparkMax(0, MotorType.kBrushless);
+    CANSparkMax elevatorMotorTwo = new CANSparkMax(0, MotorType.kBrushless);
     
-    AbsoluteEncoder shooterEncoder = new AbsoluteEncoder();
+    AbsoluteEncoder shooterEncoderOne = elevatorMotorOne.getAbsoluteEncoder(Type.kDutyCycle);
+    AbsoluteEncoder shooterEncoderTwo = elevatorMotorTwo.getAbsoluteEncoder(Type.kDutyCycle);
+
+    // Could cange depending on which switch is used
+    DigitalInput elevatorLimitSwitch = new DigitalInput(0);
 
     PIDController pid = new PIDController(0, 0, 0);
 
@@ -17,5 +26,9 @@ public class ElevatorSubsystem {
 
     public void setToIntakeMode() {
 
+    }
+
+    public void setToShooterMode() {
+        
     }
 }
