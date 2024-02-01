92729bf8ee87ccfc7b8c7ed1786a04e7b7c6cd64
manmadeofpolkadots <147563913+manmadeofpolkadots@users.noreply.github.com>
2024-01-31 20:30:48 -0500

    modified:   src/main/java/frc/robot/subsystems/ElevatorSubsystem.java

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 public class ElevatorSubsystem {
     CANSparkMax elevatorMotorOne = new CANSparkMax(0, MotorType.kBrushless);
     CANSparkMax elevatorMotorTwo = new CANSparkMax(0, MotorType.kBrushless);
-    
+
     AbsoluteEncoder shooterEncoderOne = elevatorMotorOne.getAbsoluteEncoder(Type.kDutyCycle);
     AbsoluteEncoder shooterEncoderTwo = elevatorMotorTwo.getAbsoluteEncoder(Type.kDutyCycle);
 
@@ -19,29 +19,33 @@ public class ElevatorSubsystem {
 
     DigitalInput elevatorBottomLimitSwitch = new DigitalInput(0);
 
-    }
-
     PIDController pid = new PIDController(0, 0, 0);
 
-    //The angle for shooting in the amp (currently set to 0)
+    // The angle for shooting in the amp (currently set to 0)
     double ampAngle = 0;
 
     // Sets the intake, shooter, and transit to the postion that we want it to be in
     public void setShooterAngle(double angleDegrees) {
 
     }
-    // Shifts the intake, shooter, and transit to the default postion that makes it easier pick up notes
+
+    // Shifts the intake, shooter, and transit to the default postion that makes it
+    // easier pick up notes
     public void setToIntakeMode() {
 
     }
-    // Shifts the intake, shooter, and transit to the position used for shooting into an amp
+
+    // Shifts the intake, shooter, and transit to the position used for shooting
+    // into an amp
     public void setToAmpMode() {
-        
+
     }
+
     // Tells us when the elevator has hit the top of it's height
     public void readTopLimitSwitch() {
 
     }
+
     // Tells us when the elevator has hit the bottom of it's height
     public void readBottomLimitSwitch() {
 
