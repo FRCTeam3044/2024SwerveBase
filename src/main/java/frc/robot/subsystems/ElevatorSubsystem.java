ed9df8eb1bcd120b28aebb29386c7c97b071eb85
manmadeofpolkadots <147563913+manmadeofpolkadots@users.noreply.github.com>
2024-01-31 19:49:02 -0500

    Updated Elevator Subsystem comments again

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -15,20 +15,35 @@ public class ElevatorSubsystem {
     AbsoluteEncoder shooterEncoderOne = elevatorMotorOne.getAbsoluteEncoder(Type.kDutyCycle);
     AbsoluteEncoder shooterEncoderTwo = elevatorMotorTwo.getAbsoluteEncoder(Type.kDutyCycle);
 
-    // Could change depending on which switch is used
-    DigitalInput elevatorLimitSwitch = new DigitalInput(0);
+    DigitalInput elevatorTopLimitSwitch = new DigitalInput(0);
+
+    DigitalInput elevatorBottomLimitSwitch = new DigitalInput(0);
+
+    }
 
     PIDController pid = new PIDController(0, 0, 0);
-    // Sets the intake, shooter, and transit to the postion that we want it to be int
-    public void setAngleAdjustment() {
+
+    //The angle for shooting in the amp (currently set to 0)
+    double ampAngle = 0;
+
+    // Sets the intake, shooter, and transit to the postion that we want it to be in
+    public void setShooterAngle(double angleDegrees) {
 
     }
     // Shifts the intake, shooter, and transit to the default postion that makes it easier pick up notes
     public void setToIntakeMode() {
 
     }
-    //// Shifts the intake, shooter, and transit to the default postion that makes it easier shoot notes
-    public void setToShooterMode() {
+    // Shifts the intake, shooter, and transit to the position used for shooting into an amp
+    public void setToAmpMode() {
         
+    }
+    // Tells us when the elevator has hit the top of it's height
+    public void readTopLimitSwitch() {
+
+    }
+    // Tells us when the elevator has hit the bottom of it's height
+    public void readBottomLimitSwitch() {
+
     }
 }
