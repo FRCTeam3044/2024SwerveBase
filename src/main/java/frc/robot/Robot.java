9547a70cca07dc448765e6e22fc097c705f901c2
manmadeofpolkadots <147563913+manmadeofpolkadots@users.noreply.github.com>
2024-02-03 11:11:56 -0500

    Changed CommandXbox Controller again.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -108,8 +108,8 @@ public class Robot extends LoggedRobot {
   public void teleopPeriodic() {
     m_robotContainer.climber.leftArm(0);
     m_robotContainer.climber.rightArm(0);
-    m_robotContainer.m_operatorController.getLeftBumper();
-    m_robotContainer.m_operatorController.getRightBumper();
+    m_robotContainer.m_operatorController.
+    m_robotContainer.m_operatorController.
   }
 
   @Override
