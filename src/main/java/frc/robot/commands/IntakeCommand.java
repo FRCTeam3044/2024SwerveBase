7cebb041d1d18daf87b66e1f18a76150dff9ec31
manmadeofpolkadots <147563913+manmadeofpolkadots@users.noreply.github.com>
2024-02-05 19:47:24 -0500

    Worked on intake command

---
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -0,0 +1,24 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class IntakeCommand extends Command {
+    private final IntakeSubsystem m_intake;
+    private final XboxController m_controller;
+
+    public IntakeCommand(IntakeSubsystem intake, XboxController controller) {
+        m_intake = intake;
+        m_controller = controller;
+    }
+    
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public 
+
+}
