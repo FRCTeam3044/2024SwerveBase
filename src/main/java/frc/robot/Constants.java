7e4a7b88bd5a46e4153d8645a7c1dec5fbceff34
manmadeofpolkadots <147563913+manmadeofpolkadots@users.noreply.github.com>
2024-02-05 20:37:56 -0500

    Worked on intake constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -68,6 +68,12 @@ public final class Constants {
     public static final boolean kGyroReversed = false;
   }
 
+  public static final class CANConstants {
+    public static final int kIntakeTopMotorPort = 0;
+    public static final int kIntakeBottomMotorPort = 0;
+
+  }
+
   public static final class ModuleConstants {
     // The MAXSwerve module can be configured with one of three pinion gears: 12T,
     // 13T, or 14T.
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,10 +3,12 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import frc.robot.Constants.CANConstants;
+
 public class IntakeSubsystem {
     // Defines the motor
-    CANSparkMax intakeTopMotor = new CANSparkMax(0, MotorType.kBrushless);
-    CANSparkMax intakeBottomMotor = new CANSparkMax(0, MotorType.kBrushless);
+    CANSparkMax intakeTopMotor = new CANSparkMax(CANConstants.kIntakeTopMotorPort, MotorType.kBrushless);
+    CANSparkMax intakeBottomMotor = new CANSparkMax(CANConstants.kIntakeBottomMotorPort, MotorType.kBrushless);
 
     // This will be set to true if the intake is running
     boolean isIntakeRunning = false;
@@ -26,8 +28,7 @@ public class IntakeSubsystem {
     public void consumeIntakeInput(boolean isTheBButtonPressed) {
         if (isTheBButtonPressed) {
             runIntake();
-        }
-        else {
+        } else {
             stopIntake();
         }
     }
