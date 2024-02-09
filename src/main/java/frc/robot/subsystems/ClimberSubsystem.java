fa0fa05616f88017225d7047ae289fe0466f0f2f
manmadeofpolkadots <147563913+manmadeofpolkadots@users.noreply.github.com>
2024-02-08 20:33:03 -0500

    Continued working on Transit

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -3,8 +3,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.CAN;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANConstants;
 
@@ -13,7 +11,7 @@ public class ClimberSubsystem extends SubsystemBase {
     TalonSRX rightClimberMotor = new TalonSRX(CANConstants.kClimberRightClimberMotorPort);
     // Encoder leftClimberEncoder = new Encoder(null, null);
     // Encoder rightClimberEncoder = new Encoder(null, null);
-    
+
     // power going into each of the motors
     double leftMotorPower = 0;
     double rightMotorPower = 0;
