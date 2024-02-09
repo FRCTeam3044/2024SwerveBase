b148e6a77299b565619116f303a7f81683688667
nab138 <nab@nabdev.me>
2024-02-09 15:05:36 +0000

    Go to point with supplied rotation command

--- src/main/java/frc/robot/commands/drive/FollowTrajectoryCommand.java
+++ src/main/java/frc/robot/commands/drive/FollowTrajectoryCommand.java
@@ -16,6 +16,7 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
 import java.util.function.Supplier;
@@ -45,8 +46,45 @@ public class FollowTrajectoryCommand extends Command {
     private final Timer m_timer = new Timer();
     private final Trajectory m_trajectory;
     private final HolonomicDriveController m_controller;
+    private final Supplier<Rotation2d> m_desiredRotation;
     private final Supplier<Double> m_desiredRotationSpeed;
     private final DriveSubsystem m_driveSubsystem;
+    private final boolean useSuppliedRotSpeed;
+
+    /**
+     * Constructs a new FollowTrajectory command that when executed will follow the
+     * provided trajectory. This command will not return output voltages but rather
+     * raw module states from the position controllers which need to be put into a
+     * velocity PID.
+     *
+     * <p>
+     * Note: The controllers will *not* set the outputVolts to zero upon completion
+     * of the path-
+     * this is left to the user, since it is not appropriate for paths with
+     * nonstationary endstates.
+     *
+     * @param trajectory      The trajectory to follow.
+     * @param controller      The HolonomicDriveController for the drivetrain.
+     * @param desiredRotation The angle that the drivetrain should be facing.
+     *                        This is sampled at each
+     *                        time step.
+     * @param driveSubsystem  The subsystem to use to drive the robot.
+     * @param requirements    The subsystems to require.
+     */
+    public FollowTrajectoryCommand(
+            Trajectory trajectory,
+            HolonomicDriveController controller,
+            Supplier<Rotation2d> desiredRotation,
+            DriveSubsystem driveSubsystem,
+            SubsystemBase... requirements) {
+        m_trajectory = requireNonNullParam(trajectory, "trajectory", "SwerveControllerCommand");
+        m_controller = requireNonNullParam(controller, "controller", "SwerveControllerCommand");
+        m_driveSubsystem = requireNonNullParam(driveSubsystem, "driveSubsystem", "SwerveControllerCommand");
+        m_desiredRotation = requireNonNullParam(desiredRotation, "desiredRotation", "SwerveControllerCommand");
+        m_desiredRotationSpeed = null;
+        useSuppliedRotSpeed = false;
+        addRequirements(requirements);
+    }
 
     /**
      * Constructs a new FollowTrajectory command that when executed will follow the
@@ -78,6 +116,8 @@ public class FollowTrajectoryCommand extends Command {
         m_driveSubsystem = requireNonNullParam(driveSubsystem, "driveSubsystem", "SwerveControllerCommand");
         m_desiredRotationSpeed = requireNonNullParam(desiredRotationSpeed, "desiredRotationSpeed",
                 "SwerveControllerCommand");
+        m_desiredRotation = () -> new Rotation2d();
+        useSuppliedRotSpeed = true;
         addRequirements(requirements);
     }
 
@@ -90,9 +130,14 @@ public class FollowTrajectoryCommand extends Command {
     public void execute() {
         double curTime = m_timer.get();
         var desiredState = m_trajectory.sample(curTime);
+        Rotation2d desiredRotation = m_desiredRotation.get();
         ChassisSpeeds targetChassisSpeeds = m_controller.calculate(m_driveSubsystem.getPose(), desiredState,
-                new Rotation2d());
-        targetChassisSpeeds.omegaRadiansPerSecond = m_desiredRotationSpeed.get();
+                desiredRotation);
+        if (useSuppliedRotSpeed) {
+            targetChassisSpeeds.omegaRadiansPerSecond = m_desiredRotationSpeed.get();
+        } else {
+            targetChassisSpeeds.omegaRadiansPerSecond *= -1;
+        }
         m_driveSubsystem.setDesiredChassisSpeeds(targetChassisSpeeds);
     }
 
