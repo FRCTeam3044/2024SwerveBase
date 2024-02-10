d96733c0d88e132c0f00b7d12d9d09153628ec6e
ghorrocks217 <76536843+ghorrocks217@users.noreply.github.com>
2024-02-09 19:32:29 -0500

    Fixed indents

--- src/main/java/frc/robot/commands/ElevatorSetAngleForAmpCommand.java
+++ src/main/java/frc/robot/commands/ElevatorSetAngleForAmpCommand.java
@@ -1,32 +1,30 @@
-    package frc.robot.commands;
+package frc.robot.commands;
 
-    import com.revrobotics.SparkPIDController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
 
-    import edu.wpi.first.wpilibj.XboxController;
-    import edu.wpi.first.wpilibj2.command.Command;
-    import frc.robot.subsystems.ElevatorSubsystem;
+public class ElevatorSetAngleForAmpCommand extends Command {
+    private final ElevatorSubsystem m_elevator;
+    public double ampAngle;
 
-    public class ElevatorSetAngleForAmpCommand extends Command {
-        private final ElevatorSubsystem m_elevator;
-        public double ampAngle;
-
-        public ElevatorSetAngleForAmpCommand(ElevatorSubsystem elevator, XboxController controller) {
-            m_elevator = elevator;
-            addRequirements(m_elevator);
-        }
-    
-        @Override
-        public boolean isFinished() {
-            return false;
-        }
-
-        @Override
-        public void initialize() {
-            ampAngle = m_elevator.ampAngle;
-        }
-
-        @Override
-        public void execute() {
-            m_elevator.pidHandler(ampAngle);
-        }
+    public ElevatorSetAngleForAmpCommand(ElevatorSubsystem elevator, XboxController controller) {
+        m_elevator = elevator;
+        addRequirements(m_elevator);
     }
+    
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void initialize() {
+        ampAngle = m_elevator.ampAngle;
+    }
+
+    @Override
+    public void execute() {
+        m_elevator.pidHandler(ampAngle);
+    }
+}
