79fa65e3e23e22f8bc95adca9d2c7964429abdb7
nab138 <NicholasDanielSharp@gmail.com>
2024-02-13 20:25:48 -0500

    Remove Unused Imports

--- src/main/java/frc/robot/utils/StateMachine.java
+++ src/main/java/frc/robot/utils/StateMachine.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.PathfindingTargets;
