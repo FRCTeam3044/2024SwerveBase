b9d31269ceb03017af8b8f822dbf420e97b462da
nab138 <NicholasDanielSharp@gmail.com>
2024-02-16 19:45:24 -0500

    Bucketing!!!

--- src/main/java/frc/robot/utils/AutoAiming.java
+++ src/main/java/frc/robot/utils/AutoAiming.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -14,18 +15,18 @@ import org.json.JSONTokener;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
+import me.nabdev.oxconfig.ConfigurableParameter;
 
 public class AutoAiming {
     public static final int degree = 3;
     public static final boolean isCollecting = true;
+    public static final ConfigurableParameter<Double> bucketSize = new ConfigurableParameter<>(0.1, "Bucket Size");
 
     private boolean runningWithoutData = false;
     private PolynomialRegression polynomialRegression;
     private JSONArray data;
     private double[] distances;
     private double[] angles;
-
-    // /U/toiwehiotwegoweiguowehoigwihobwuogf.json
 
     public AutoAiming() throws FileNotFoundException {
         FileInputStream fis = new FileInputStream(new File(getPath()));
@@ -79,15 +80,49 @@ public class AutoAiming {
             distancesTemp.add(point.getDouble("distance"));
             anglesTemp.add(point.getDouble("angle"));
         }
+        double longestDistance = 0;
         distances = new double[distancesTemp.size()];
         angles = new double[anglesTemp.size()];
         for (int i = 0; i < distancesTemp.size(); i++) {
             distances[i] = distancesTemp.get(i);
+            if (distances[i] > longestDistance) {
+                longestDistance = distances[i];
+            }
             angles[i] = anglesTemp.get(i);
         }
 
+        int bucketCount = (int) Math.ceil(longestDistance / bucketSize.get()) + 1;
+        ArrayList<ArrayList<Double>> distanceBuckets = new ArrayList<>();
+        ArrayList<ArrayList<Double>> angleBuckets = new ArrayList<>();
+        for (int i = 0; i < bucketCount; i++) {
+            distanceBuckets.add(new ArrayList<>());
+            angleBuckets.add(new ArrayList<>());
+        }
+        for (int i = 0; i < distances.length; i++) {
+            int bucket = (int) Math.floor(distances[i] / bucketSize.get());
+            distanceBuckets.get(bucket).add(distances[i]);
+            angleBuckets.get(bucket).add(angles[i]);
+        }
+
+        ArrayList<Double> medianDistances = new ArrayList<>();
+        ArrayList<Double> medianAngles = new ArrayList<>();
+        // Add the median of each bucket to the arrays
+        for (int i = 0; i < bucketCount; i++) {
+            if (distanceBuckets.get(i).size() == 0) {
+                continue;
+            }
+            medianDistances.add(median(distanceBuckets.get(i)));
+            medianAngles.add(median(angleBuckets.get(i)));
+        }
+
+        double[] processedDistances = new double[medianDistances.size()];
+        double[] processedAngles = new double[medianAngles.size()];
+        for (int i = 0; i < medianDistances.size(); i++) {
+            processedDistances[i] = medianDistances.get(i);
+            processedAngles[i] = medianAngles.get(i);
+        }
         try {
-            polynomialRegression = new PolynomialRegression(distances, angles, degree);
+            polynomialRegression = new PolynomialRegression(processedDistances, processedAngles, degree);
             runningWithoutData = false;
         } catch (IllegalArgumentException e) {
             DriverStation.reportWarning("Not enough data to create a polynomial regression", false);
@@ -105,6 +140,16 @@ public class AutoAiming {
             return isCollecting ? "/U/auto-aiming.json" : Filesystem.getDeployDirectory() + "/auto-aiming.json";
         } else {
             return Filesystem.getOperatingDirectory() + "/auto-aiming-sim.json";
+        }
+    }
+
+    private double median(ArrayList<Double> list) {
+        Collections.sort(list);
+        int length = list.size();
+        if (length % 2 == 0) {
+            return (list.get(length / 2) + list.get(length / 2 - 1)) / 2;
+        } else {
+            return list.get(length / 2);
         }
     }
 }
