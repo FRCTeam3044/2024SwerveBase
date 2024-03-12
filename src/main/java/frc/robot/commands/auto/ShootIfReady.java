87087b952c5daa48d843e995643a61aa7d432dc1
nab138 <NicholasDanielSharp@gmail.com>
2024-03-12 14:48:47 -0400

    Auto stuff

--- src/main/java/frc/robot/commands/auto/ShootIfReady.java
+++ src/main/java/frc/robot/commands/auto/ShootIfReady.java
@@ -1,31 +1,23 @@
 package frc.robot.commands.auto;
 
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TransitSubsystem;
 import frc.robot.subsystems.StateMachine.State;
-import frc.robot.utils.AutoTargetUtils;
-import me.nabdev.oxconfig.ConfigurableParameter;
 
 public class ShootIfReady extends Command {
     private TransitSubsystem m_transit;
-    private DriveSubsystem m_drive;
-    private ConfigurableParameter<Double> m_shooterSpinupRange = new ConfigurableParameter<Double>(7.5,
-            "Shooter Spinup Range");
 
-    public ShootIfReady(TransitSubsystem transit, DriveSubsystem drive) {
-        m_drive = drive;
+    public ShootIfReady(TransitSubsystem transit) {
         m_transit = transit;
         addRequirements(m_transit);
     }
 
     @Override
     public void execute() {
-        if(RobotContainer.stateMachine.getState() === State.READY_TO_SHOOT) {
-            m_transit.shoot();
+        if (m_transit != null &&
+                RobotContainer.stateMachine.getState() == State.READY_TO_SHOOT) {
+            m_transit.runTransit();
         }
     }
 
@@ -36,7 +28,7 @@ public class ShootIfReady extends Command {
 
     @Override
     public void end(boolean interrupted) {
-
+        m_transit.stopTransit();
     }
 
 }
