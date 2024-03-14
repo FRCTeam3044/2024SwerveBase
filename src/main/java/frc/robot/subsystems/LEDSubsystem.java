ef4d9419389ce5a03a60a41d821974311774bd0a
nab138 <NicholasDanielSharp@gmail.com>
2024-03-14 13:00:52 -0400

    LEDs?

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -15,16 +15,17 @@ public class LEDSubsystem extends SubsystemBase {
     private int m_RainbowFirstPixelHue;
     private RobotContainer m_robotContainer;
     private Timer timer = new Timer();
-    
+
     /**
      * Creates a new LEDSubsystem to control Addressable LEDs
-     * @param port The PWM Port of the LEDs
+     * 
+     * @param port      The PWM Port of the LEDs
      * @param ledLength The number of LEDs on the strand
      */
     public LEDSubsystem(int port, int ledLength, RobotContainer robotContainer) {
         m_robotContainer = robotContainer;
         m_led = new AddressableLED(port);
-        m_ledBuffer = new AddressableLEDBuffer(52);
+        m_ledBuffer = new AddressableLEDBuffer(ledLength);
         m_led.setLength(m_ledBuffer.getLength());
         m_led.setData(m_ledBuffer);
         m_led.start();
@@ -38,9 +39,10 @@ public class LEDSubsystem extends SubsystemBase {
     }
 
     public void setRainbow() {
-        for (var i = LEDConstants.sideLEDLength + 1; i < LEDConstants.topLEDLength; i++) {
-          final var hue = (m_RainbowFirstPixelHue + (i * 180 / LEDConstants.topLEDLength)) % 180;
-          m_ledBuffer.setHSV(i, hue, 255, 64);
+        for (var i = LEDConstants.LEDOffset; i < m_ledBuffer.getLength(); i++) {
+            final var hue = (m_RainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength() - LEDConstants.LEDOffset))
+                    % 180;
+            m_ledBuffer.setHSV(i, hue, 255, 64);
         }
         m_RainbowFirstPixelHue += 3;
         m_RainbowFirstPixelHue %= 180;
@@ -55,12 +57,12 @@ public class LEDSubsystem extends SubsystemBase {
     }
 
     public void blinkColorTop(int r, int g, int b, double time) {
-        if(timer.get() == 0) {
+        if (timer.get() == 0) {
             timer.start();
-        } else if(timer.get() >= time) {
+        } else if (timer.get() >= time) {
             timer.reset();
         }
-        if(timer.get() < time / 2) {
+        if (timer.get() < time / 2) {
             for (var i = 0; i < LEDConstants.topLEDLength; i++) {
                 m_ledBuffer.setRGB(LEDConstants.LEDOffset + i, r, g, b);
             }
@@ -88,22 +90,22 @@ public class LEDSubsystem extends SubsystemBase {
         // setRainbow();
         // setSidesRainbow();
         // if(m_robotContainer.stateMachineCommand.isScheduled()) {
-        //     setSidesRainbow();
+        // setSidesRainbow();
         // } else {
-        //     setSidesRainbow();
+        // setSidesRainbow();
         // }
 
         // State state = RobotContainer.stateMachine.getState();
         // if(state == State.NOTE_LOADED) {
-        //     setColorTop(255, 165, 0);
+        // setColorTop(255, 165, 0);
         // } else if(state == State.OWNS_NOTE) {
-        //     blinkColorTop(255,165,0, 0.250);
+        // blinkColorTop(255,165,0, 0.250);
         // } else if(state == State.TARGETING_NOTE) {
-        //     if(RobotContainer.m_noteDetection.hasNote) {
-        //         setCompass(RobotContainer.m_noteDetection.getClosestNoteCameraXPosition());
-        //     }
+        // if(RobotContainer.m_noteDetection.hasNote) {
+        // setCompass(RobotContainer.m_noteDetection.getClosestNoteCameraXPosition());
+        // }
         // } else if(state == State.READY_TO_SHOOT) {
-        //     setColorTop(0, 255, 0);
+        // setColorTop(0, 255, 0);
         // }
     }
 }
