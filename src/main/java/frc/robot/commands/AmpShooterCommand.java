1b5d2637463f1a598064e1464e12cd00f831e139
nab138 <NicholasDanielSharp@gmail.com>
2024-03-21 08:14:49 -0400

    Cleanup shooter commands

--- src/main/java/frc/robot/commands/AmpShooterCommand.java
+++ src/main/java/frc/robot/commands/AmpShooterCommand.java
@@ -1,30 +1,20 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TransitSubsystem;
-import me.nabdev.oxconfig.ConfigurableParameter;
 
 public class AmpShooterCommand extends Command {
     private final ShooterSubsystem m_shooter;
-    private final TransitSubsystem m_transit;
-    private final Timer m_timer = new Timer();
-    private final ConfigurableParameter<Double> m_ampShooterSpinupTime = new ConfigurableParameter<Double>(
-            2.0, "Amp Shooter Spinup Time");
-    private boolean ranTransit = false;
 
     public AmpShooterCommand(ShooterSubsystem shooter, TransitSubsystem transit) {
         m_shooter = shooter;
-        m_transit = transit;
         addRequirements(m_shooter);
     }
 
     @Override
     public void initialize() {
-        ranTransit = false;
-        m_timer.reset();
-        m_timer.start();
+
     }
 
     @Override
@@ -38,16 +28,10 @@ public class AmpShooterCommand extends Command {
         m_shooter.handlePID();
         // m_shooter.consumeShooterInput(true, false);
         // if(m_shooter.shooterAtSpeed()){
-        if (m_timer.get() > m_ampShooterSpinupTime.get()) {
-            ranTransit = true;
-            // m_transit.runTransit();
-        }
     }
 
     @Override
     public void end(boolean interrupted) {
         m_shooter.consumeShooterInput(false, false);
-        if (ranTransit)
-            m_transit.stopTransit();
     }
 }
--- src/main/java/frc/robot/commands/ManualLobCommand.java
+++ src/main/java/frc/robot/commands/ManualLobCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.TransitSubsystem;
@@ -8,26 +7,20 @@ import me.nabdev.oxconfig.ConfigurableParameter;
 
 public class ManualLobCommand extends Command {
     private final ShooterSubsystem m_shooter;
-    private final TransitSubsystem m_transit;
-    private final Timer m_timer = new Timer();
     // private final ConfigurableParameter<Double> m_shooterSpinupTime = new
     // ConfigurableParameter<Double>(
     // 2.0, "Shooter Spinup Time");
-    private boolean ranTransit = false;
     private final ConfigurableParameter<Double> m_shooterLobSpeed = new ConfigurableParameter<Double>(
             2000.0, "Lob Speed");
 
     public ManualLobCommand(ShooterSubsystem shooter, TransitSubsystem transit) {
         m_shooter = shooter;
-        m_transit = transit;
         addRequirements(m_shooter);
     }
 
     @Override
     public void initialize() {
-        ranTransit = false;
-        m_timer.reset();
-        m_timer.start();
+
     }
 
     @Override
@@ -50,7 +43,5 @@ public class ManualLobCommand extends Command {
     @Override
     public void end(boolean interrupted) {
         m_shooter.consumeShooterInput(false, false);
-        if (ranTransit)
-            m_transit.stopTransit();
     }
 }
